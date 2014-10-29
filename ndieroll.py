31d8f08354999f7fb5a591b1ada0f5908590edd5
Joshua Peterson <joshm.peterson@gmail.com>
2014-10-29 12:41:29 -0700

    Addition of check and reprompt for user input

--- ndieroll.py
+++ ndieroll.py
@@ -9,9 +9,19 @@ die.
 """
 # N -- max integer on the die
 N = int(raw_input("How many sides does the die have? N = "))
+
+# while improper number of sides is given, prompt for proper input
+while N <= 0:
+	N = int(raw_input("Please provide a positive integer for the number of sides. N = "))
+
 # n -- number of times to roll the die
 n = int(raw_input("How many times do you want to roll the %d-sided die? n = " % N))
 
+# while improper input is given, prompt for proper input
+while n <= 0:
+	n = int(raw_input("A postive integer value is required for the number of rolls. n = "))
+
+print "The following are %d simulated die rolls of a %d-sided die:" % (n,N)
 
 for i in range(0, n):
 	print random.randint(1,N)
