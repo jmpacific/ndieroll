8c298bd56a36a82c7022e3277aa135999862561f
Joshua Peterson <joshm.peterson@gmail.com>
2014-10-29 14:18:33 -0700

    Simulations continue until user quit

--- ndieroll.py
+++ ndieroll.py
@@ -7,22 +7,39 @@ number of sides for the die, and 'n',
 the number of times to roll said N-sided
 die.
 """
-# N -- max integer on the die
-N = int(raw_input("How many sides does the die have? N = "))
 
-# while improper number of sides is given, prompt for proper input
-while N <= 0:
-	N = int(raw_input("Please provide a positive integer for the number of sides. N = "))
+def dieSim():
+    # N -- max integer on the die
+    N = int(raw_input("How many sides does the die have? N = "))
 
-# n -- number of times to roll the die
-n = int(raw_input("How many times do you want to roll the %d-sided die? n = " % N))
+    # while improper number of sides is given, prompt for proper input
+    while N <= 0:
+        N = int(raw_input("Please provide a positive integer for the number of sides. N = "))
 
-# while improper input is given, prompt for proper input
-while n <= 0:
-	n = int(raw_input("A postive integer value is required for the number of rolls. n = "))
+    # n -- number of times to roll the die
+    n = int(raw_input("How many times do you want to roll the %d-sided die? n = " % N))
 
-print "The following are %d simulated die rolls of a %d-sided die:" % (n,N)
+    # while improper input is given, prompt for proper input
+    while n <= 0:
+        n = int(raw_input("A postive integer value is required for the number of rolls. n = "))
 
-for i in range(0, n):
-	print random.randint(1,N)
+    print "The following are %d simulated die rolls of a %d-sided die:" % (n,N)
+
+    for i in range(0, n):
+        print random.randint(1,N)
+
+# Run the simulator a first time
+dieSim()
+
+# Prompt user to rerun simulator
+userQuit = raw_input("Would you like to roll again? (Yes/No) ")
+while userQuit != 'Yes' and userQuit != 'No':
+    userQuit = raw_input("Please type 'Yes' or 'No'. ")
+
+# Run until user inputs 'No'
+while userQuit == 'Yes':
+
+    dieSim()
+
+
 
