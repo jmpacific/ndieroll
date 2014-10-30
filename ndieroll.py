612bdf2d37b8375d394ce8fff715c5b59d822d3b
Joshua Peterson <joshm.peterson@gmail.com>
2014-10-30 09:53:28 -0700

    Release version 1.0

--- ndieroll.py
+++ ndieroll.py
@@ -1,20 +1,34 @@
+# ndieroll.py
+#
+# N-Die-Roll simulator
+#
+# by jmpacific
 import random
+import sys
 
-print """
-Welcome to the N-Die-Roll simulator.
-You will be asked to supply 'N', the
-number of sides for the die, and 'n',
-the number of times to roll said N-sided
-die.
-"""
+def getDieRolls(N,n):
+# returns a list of n die rolls of die with N number of sides.
+    results = []    
 
-def dieSim():
+    for i in range(0,n):
+        results.append(random.randint(1,N))
+
+    return results
+# end def getDieRolls()
+
+def userInput():
+# Ask user for the number of sides for simulated die
+# Ask user for the number of times to roll a die with sides N
+
     # N -- max integer on the die
     N = int(raw_input("How many sides does the die have? N = "))
 
     # while improper number of sides is given, prompt for proper input
     while N <= 0:
         N = int(raw_input("Please provide a positive integer for the number of sides. N = "))
+
+    isPlatonic(N)
+        
 
     # n -- number of times to roll the die
     n = int(raw_input("How many times do you want to roll the %d-sided die? n = " % N))
@@ -23,23 +37,57 @@ def dieSim():
     while n <= 0:
         n = int(raw_input("A postive integer value is required for the number of rolls. n = "))
 
-    print "The following are %d simulated die rolls of a %d-sided die:" % (n,N)
+    return(N,n)
+# end def userInput()
 
-    for i in range(0, n):
-        print random.randint(1,N)
+def isPlatonic(x):
+# Checks to see if the number of sides for the die is a Platonic solid
+    if x not in [4,6,8,12,20]:
+        print "This is not a Platonic solid; just saying..."
 
-# Run the simulator a first time
-dieSim()
+def promptQuit():
+    choice = raw_input("Do you want to roll again? (yes/no) ")
+    while choice != 'yes' and choice != 'no':
+        choice = raw_input("Please answer 'yes' or 'no': ")
+    return choice
+# end def promptQuit()
 
-# Prompt user to rerun simulator
-userQuit = raw_input("Would you like to roll again? (Yes/No) ")
-while userQuit != 'Yes' and userQuit != 'No':
-    userQuit = raw_input("Please type 'Yes' or 'No'. ")
+def runDieSim(x):
 
-# Run until user inputs 'No'
-while userQuit == 'Yes':
+    rolls = getDieRolls(x[0],x[1])
 
-    dieSim()
+    print "The die rolls are:"
+    for i in rolls:
+        print i
 
+# end def runDieSim()
 
+#####
+# Run the simulator given no command line arguments
+#####
 
+if len(sys.argv) >= 3:
+    print "\nUsing command line arguments for dies sides and rolls..."
+    x = []
+    x.append(int(sys.argv[1]))
+    x.append(int(sys.argv[2]))
+    isPlatonic(x[0])
+    runDieSim(x)
+else:
+
+    print """
+    Welcome to the N-Die-Roll simulator.
+    You will be asked to supply 'N', the
+    number of sides for the die, and 'n',
+    the number of times to roll said N-sided
+    die.
+    """
+
+    rollAgain = 'yes'
+
+    while rollAgain == 'yes':
+
+        x = userInput()
+        runDieSim(x)
+
+        rollAgain = promptQuit()
